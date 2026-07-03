5173a892dc3ab412564eb337855ffd3087565265
Herdian Iskandar <herdian.iskandar99@gmail.com>
2026-07-03 16:36:25 +0700

    fitur departments - index and create

---
+++ app/Http/Controllers/DepartmentController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Department;
+use Illuminate\Http\Request;
+
+class DepartmentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $departments = Department::all();
+
+        return view('departments.index', compact('departments'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+
+        return view('departments.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'status' => 'required|in:active,inactive',
+        ]);
+
+        Department::create($request->all());
+
+        return redirect()->route('departments.index')->with('success', 'Department created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Department $department)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Department $department)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Department $department)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Department $department)
+    {
+        //
+    }
+}
