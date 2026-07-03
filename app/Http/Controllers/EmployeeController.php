c6318d3fc930222638b7a0da0fb683ba107e896a
Herdian Iskandar <herdian.iskandar99@gmail.com>
2026-07-03 16:04:34 +0700

    fitur employees - delete data

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -100,6 +100,8 @@ class EmployeeController extends Controller
      */
     public function destroy(Employee $employee)
     {
-        //
+        $employee->delete();
+
+        return redirect()->route('employees.index')->with('success', 'Employee deleted successfully.');
     }
 }
