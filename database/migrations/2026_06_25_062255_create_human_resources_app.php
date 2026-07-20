ed29d22b6be93840b294b6f57985a46dfdbba490
Herdian Iskandar <herdian.iskandar99@gmail.com>
2026-07-20 21:20:54 +0700

    update checkout field pada table presences

--- database/migrations/2026_06_25_062255_create_human_resources_app.php
+++ database/migrations/2026_06_25_062255_create_human_resources_app.php
@@ -88,7 +88,7 @@ return new class extends Migration
             $table->foreignId('employee_id')->constrained('employees');
 
             $table->date('check_in');
-            $table->date('check_out');
+            $table->date('check_out')->nullable();
             $table->date('date');
             $table->string('status');
 
